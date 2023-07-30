1391d98e58cf36e10ef4723d45bc861fecbf9730
imujo <ivo.mujo.3@gmail.com>
2023-07-30 11:22:40 +0200

    Fix hook order

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -3,15 +3,14 @@ import { useMediaQuery } from "react-responsive";
 
 export type ScreenSize = "2xl" | "xl" | "lg" | "md" | "sm" | "xs";
 
-export default function useScreenSize() {
-  if (!window) return null;
-
+export default function useScreenSize(): ScreenSize | null {
   const xxl = useMediaQuery({ minWidth: 1536 });
   const xl = useMediaQuery({ minWidth: 1280 });
   const lg = useMediaQuery({ minWidth: 1024 });
   const md = useMediaQuery({ minWidth: 768 });
   const sm = useMediaQuery({ minWidth: 640 });
 
+  if (!window) return null;
   if (xxl) return "2xl";
   if (xl) return "xl";
   if (lg) return "lg";
