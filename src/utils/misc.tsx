1f945be453c18f9fa8cb5240bdf52e9f36fd528f
imujo <ivo.mujo.3@gmail.com>
2023-07-30 10:40:04 +0200

    Fix env url formating

--- src/utils/misc.tsx
+++ src/utils/misc.tsx
@@ -8,3 +8,13 @@ export function cn(...classes: ClassValue[]) {
 export function twoDigitNumber(number: number) {
   return number < 10 ? "0" + number : "" + number;
 }
+
+export function joinURLs(url1: string, url2: string) {
+  return removeTrailingSlash(url1) + "/" + removeLeadingSlash(url2);
+}
+function removeLeadingSlash(url: string) {
+  return url[0] === "/" ? (url = url.substring(1)) : url;
+}
+function removeTrailingSlash(url: string) {
+  return url[url.length - 1] === "/" ? url.substring(0, url.length - 1) : url;
+}
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,14 @@
+import { joinURLs } from "./misc";
+
 export default async function request<T>(
   url: string,
   config: RequestInit = {}
 ): Promise<T> {
+  console.log("\n\n\n");
   if (!process.env.SERVER_ENDPOINT)
     throw new Error("No .env variable SERVER_ENDPOINT found!");
 
-  return fetch(process.env.SERVER_ENDPOINT + url, {
+  return fetch(joinURLs(process.env.SERVER_ENDPOINT, url), {
     next: { revalidate: 1 },
   })
     .then((response) => response.json())
